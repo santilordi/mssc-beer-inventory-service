b8bad17929328516f0337cb2235d8dffc9293314
Santiago Lordi <santilordi569@gmail.com>
2026-02-03 19:20:44 -0300

    fix missing property

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -32,6 +32,7 @@ public class AllocationListener {
             } else {
                 builder.pendingInventory(true);
             }
+            builder.allocationError(false);
         } catch (Exception e){
             log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
             builder.allocationError(true);
