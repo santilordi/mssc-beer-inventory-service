6f7bbdd6d2639880b548c236d65c5cbb3bd08121
Santiago Lordi <santilordi569@gmail.com>
2026-02-14 12:24:28 -0300

    adding Eureka client config

---
+++ src/main/java/guru/sfg/beer/inventory/service/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package guru.sfg.beer.inventory.service.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableAutoConfiguration
+@Configuration
+public class LocalDiscovery {
+}
